2dba2faff448ee68048925699d51df267fc53cb2
wind <windspeech@qq.com>
2020-06-03 21:33:31 +0800

    数据库迁移fix

--- database/migrations/2020_05_26_170026_create_categories_table.php
+++ database/migrations/2020_05_26_170026_create_categories_table.php
@@ -14,7 +14,7 @@ class CreateCategoriesTable extends Migration
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('name')->index()->comment('名称');
             $table->text('description')->nullable()->comment('描述');
             $table->integer('post_count')->default(0)->comment('帖子数');
