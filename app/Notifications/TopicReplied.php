01c93da9e144658918f6299b702b0eca183b1e57
wind <windspeech@qq.com>
2020-06-06 09:59:29 +0800

    支持队列的邮件通知 -- 未完

--- app/Notifications/TopicReplied.php
+++ app/Notifications/TopicReplied.php
@@ -32,7 +32,7 @@ class TopicReplied extends Notification
     public function via($notifiable)
     {
         // 开启通知的频道
-        return ['database'];
+        return ['database', 'mail'];
     }
 
     /**
@@ -43,10 +43,10 @@ class TopicReplied extends Notification
      */
     public function toMail($notifiable)
     {
+        $url = $this->reply->topic->link(['#reply' . $this->reply->id]);
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+                    ->line('你的话题有新回复！')
+                    ->action('查看回复', $url);
     }
 
     public function toDatabase($notifiable)
