d371650114165da313c9289a0d6c262886b6a87c
zhuozhanyang <windzhuo@goatgames.com>
2020-06-15 11:50:39 +0800

    发送短信验证码

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// 短信验证码
+Route::post('verificationCodes', 'Api\VerificationCodesController@store')
+    ->name('verificationCodes.store');
