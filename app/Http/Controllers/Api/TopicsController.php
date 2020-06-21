9100ddc234f21ee945d1d30dbe4424cca6562e7f
wind <windspeech@qq.com>
2020-06-21 18:43:27 +0800

    话题详情

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -2,65 +2,32 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Models\Topic;
 use App\Models\User;
-use Spatie\QueryBuilder\AllowedFilter;
-use Spatie\QueryBuilder\QueryBuilder;
+use App\Models\Topic;
 use Illuminate\Http\Request;
+use App\Http\Queries\TopicQuery;
 use App\Http\Resources\TopicResource;
 use App\Http\Requests\Api\TopicRequest;
 
 class TopicsController extends Controller
 {
-    public function store(TopicRequest $request, Topic $topic)
+    public function index(Request $request, TopicQuery $query)
     {
-        $topic->fill($request->all());
-        $topic->user_id = $request->user()->id;
-        $topic->save();
-
-        return new TopicResource($topic);
-    }
-    public function update(TopicRequest $request, Topic $topic)
-    {
-        $this->authorize('update', $topic);
-
-        $topic->update($request->all());
-        return new TopicResource($topic);
-    }
-    public function destroy(Topic $topic)
-    {
-        $this->authorize('destroy', $topic);
-
-        $topic->delete();
-
-        return response(null, 204);
-    }
-    public function index(Request $request, Topic $topic)
-    {
-        $topics = QueryBuilder::for(Topic::class)
-            ->allowedIncludes('user', 'category')
-            ->allowedFilters([
-                'title',
-                AllowedFilter::exact('category_id'),
-                AllowedFilter::scope('withOrder')->default('recentReplied'),
-            ])
-            ->paginate();
+        $topics = $query->paginate();
 
         return TopicResource::collection($topics);
     }
-    public function userIndex(Request $request, User $user)
-    {
-        $query = $user->topics()->getQuery();
 
-        $topics = QueryBuilder::for($query)
-            ->allowedIncludes('user', 'category')
-            ->allowedFilters([
-                'title',
-                AllowedFilter::exact('category_id'),
-                AllowedFilter::scope('withOrder')->default('recentReplied'),
-            ])
-            ->paginate();
+    public function userIndex(Request $request, User $user, TopicQuery $query)
+    {
+        $topics = $query->where('user_id', $user->id)->paginate();
 
         return TopicResource::collection($topics);
+    }
+
+    public function show($topicId, TopicQuery $query)
+    {
+        $topic = $query->findOrFail($topicId);
+        return new TopicResource($topic);
     }
 }
