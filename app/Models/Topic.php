51f66e8aa36ae49341dc1ecf1a66b89f196c4d37
zhuozhanyang <windzhuo@goatgames.com>
2020-06-24 16:46:04 +0800

    话题回复

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -62,8 +62,8 @@ class Topic extends Model
 
     public function updateReplyCount()
     {
-        $reply->topic->reply_count = $reply->topic->replies->count();
-        $reply->topic->save();
+        $this->reply_count = $this->replies->count();
+        $this->save();
     }
 }
 
